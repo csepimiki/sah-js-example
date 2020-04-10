c79ec732268baa4bf66eb04affc898bd1b0cb3f6
csepimiki <47494690+csepimiki@users.noreply.github.com>
2020-04-10 16:11:38 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+// 4th part
+
+$('p').html('<strong>hello world</strong>');
+
+$('p').append('<strong>hello world</strong>');
+
+
+$('button').click(()=> {
+    $('.first').toggleClass('hidden');
+})
+
